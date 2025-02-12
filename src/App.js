0553b872747ce2291c43941181ab55c76e4a85d4
Shkemb <sshkembkryeziu@gmail.com>
2025-02-12 19:41:47 +0100

    Eccomerce

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import React, { useEffect } from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  }, []);
+
+  return <h1>hello</h1>;
 }
 
 export default App;
